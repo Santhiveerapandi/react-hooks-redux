0bb00cfc2aebdbf2469e4b77d22712d30ae184a0
Santhiveerapandi Kamaraj <santhiveerapandi@gmail.com>
2022-02-11 17:33:19 +0530

    initial commit

---
+++ src/App/App.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { history } from '../_helpers';
+import { alertActions } from '../_actions';
+import { PrivateRoute } from '../_components';
+import { HomePage } from '../HomePage';
+import { LoginPage } from '../LoginPage';
+import { RegisterPage } from '../RegisterPage';
+
+function App() {
+    const alert = useSelector(state => state.alert);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        history.listen((location, action) => {
+            // clear alert on location change
+            dispatch(alertActions.clear());
+        });
+    }, []);
+
+    var stylingObject = {
+        div: {
+          //color: "red",
+          border: "1rem solid red",
+          padding: ".5rem",
+          "border-radius": "1.5rem",
+          "float":"center",
+          margin: "10%",
+          "margin-bottom": "0px"
+        }, input: {
+          margin: "2px",
+          padding: "5px"
+        }
+      }
+
+    return (
+        <div className="jumbotron" style={stylingObject.div} >
+            <div className="container">
+                <div className="col-md-8">
+                    {/* offset-md-2 */}
+                    {alert.message &&
+                        <div className={`alert ${alert.type}`}>{alert.message}</div>
+                    }
+                    <Router history={history}>
+                        <Switch>
+                            <PrivateRoute exact path="/" component={HomePage} />
+                            <Route path="/login" component={LoginPage} />
+                            <Route path="/register" component={RegisterPage} />
+                            <Redirect from="*" to="/" />
+                        </Switch>
+                    </Router>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export { App };
